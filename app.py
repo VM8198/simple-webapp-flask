38f89359d5a342d9f101bb5bece0d09ba60fbefb
Mohit Shah <mohitmshah09@gmail.com>
2021-06-24 19:00:13 +0530

    Changed messages

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Hello World!"
 
 @app.route('/how are you')
 def hello():
     return 'I am good too, how about you?'
+
+@app.route('/test')
+def test():
+    return 'Running well!'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
